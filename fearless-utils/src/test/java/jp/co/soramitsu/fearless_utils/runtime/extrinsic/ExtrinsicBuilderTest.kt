a6bb479a6426a3051d8d05e006aa030c63e4ef6c
valentunn <70131744+valentunn@users.noreply.github.com>
2021-11-04 11:02:33 +0300

    Add batch_all support for ExtrinsicBuilder (#16)
    * Add batch all support for ExtrinsicBuilder
    
    * Bump versions

--- fearless-utils/src/test/java/jp/co/soramitsu/fearless_utils/runtime/extrinsic/ExtrinsicBuilderTest.kt
+++ fearless-utils/src/test/java/jp/co/soramitsu/fearless_utils/runtime/extrinsic/ExtrinsicBuilderTest.kt
@@ -6,7 +6,9 @@ import jp.co.soramitsu.fearless_utils.encrypt.keypair.BaseKeypair
 import jp.co.soramitsu.fearless_utils.extensions.fromHex
 import jp.co.soramitsu.fearless_utils.integration.transfer
 import jp.co.soramitsu.fearless_utils.runtime.RealRuntimeProvider
+import jp.co.soramitsu.fearless_utils.runtime.definitions.types.fromHex
 import jp.co.soramitsu.fearless_utils.runtime.definitions.types.generics.Era
+import jp.co.soramitsu.fearless_utils.runtime.definitions.types.generics.Extrinsic
 import jp.co.soramitsu.fearless_utils.runtime.definitions.types.generics.multiAddressFromId
 import jp.co.soramitsu.fearless_utils.ss58.SS58Encoder.toAccountId
 import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.chain.RuntimeVersion
@@ -68,17 +70,7 @@ class ExtrinsicBuilderTest {
         val extrinsicInHex =
             "0x41028400fdc41550fb5186d71cae699c31731b3e1baa10680c7bd6b3831a6d222cf4d16800080bfe8bc67f44b498239887dc5679523cfcb1d20fd9ec9d6bae0a385cca118d2cb7ef9f4674d52a810feb32932d7c6fe3e05ce9e06cd72cf499c8692206410ab5038800040000340a806419d5e278172e45cb0e50da1b031795366c99ddfe0a680bd53b142c630700e40b5402"
 
-        val builder = ExtrinsicBuilder(
-            runtime = runtime,
-            keypair = KEYPAIR,
-            nonce = 34.toBigInteger(),
-            runtimeVersion = RuntimeVersion(48, 4),
-            genesisHash = "e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e".fromHex(),
-            multiChainEncryption = MultiChainEncryption.Substrate(EncryptionType.ED25519),
-            accountIdentifier = multiAddressFromId(KEYPAIR.publicKey),
-            era = Era.Mortal(64, 59),
-            blockHash = "0x1b876104c68b4a8924c098d61d2ad798761bb6fff55cca2885939ffc27ef5ecb".fromHex()
-        )
+        val builder = createExtrinsicBuilder()
 
         builder.transfer(
             recipientAccountId = "340a806419d5e278172e45cb0e50da1b031795366c99ddfe0a680bd53b142c63".fromHex(),
@@ -96,17 +88,7 @@ class ExtrinsicBuilderTest {
         val extrinsicInHex =
             "0xf1028400fdc41550fb5186d71cae699c31731b3e1baa10680c7bd6b3831a6d222cf4d168005b94d4436372ba74895936695e97d543358219e77f3e827f77b2e26f53413363a5dd098e172a51308e7d35aa6c03c5f171c4b43732db61c3d86b62d83e626b07b5038800100008040000340a806419d5e278172e45cb0e50da1b031795366c99ddfe0a680bd53b142c630700e40b5402040000340a806419d5e278172e45cb0e50da1b031795366c99ddfe0a680bd53b142c630700e40b5402"
 
-        val builder = ExtrinsicBuilder(
-            runtime = runtime,
-            keypair = KEYPAIR,
-            nonce = 34.toBigInteger(),
-            runtimeVersion = RuntimeVersion(48, 4),
-            genesisHash = "e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e".fromHex(),
-            multiChainEncryption = MultiChainEncryption.Substrate(EncryptionType.ED25519),
-            accountIdentifier = multiAddressFromId(KEYPAIR.publicKey),
-            era = Era.Mortal(64, 59),
-            blockHash = "0x1b876104c68b4a8924c098d61d2ad798761bb6fff55cca2885939ffc27ef5ecb".fromHex()
-        )
+        val builder = createExtrinsicBuilder()
 
         repeat(2) {
             builder.transfer(
@@ -119,4 +101,33 @@ class ExtrinsicBuilderTest {
 
         assertEquals(extrinsicInHex, encoded)
     }
+
+    @Test
+    fun `should build batch_all extrinsic`() {
+        val extrinsicBuilder = createExtrinsicBuilder()
+
+        repeat(2) {
+            extrinsicBuilder.transfer(
+                recipientAccountId = "340a806419d5e278172e45cb0e50da1b031795366c99ddfe0a680bd53b142c63".fromHex(),
+                amount = BigInteger("10000000000")
+            )
+        }
+
+        val encoded = extrinsicBuilder.build(useBatchAll = true)
+        val decoded = Extrinsic.fromHex(runtime, encoded)
+
+        assertEquals(decoded.call.function.name, "batch_all")
+    }
+
+    private fun createExtrinsicBuilder()  = ExtrinsicBuilder(
+        runtime = runtime,
+        keypair = KEYPAIR,
+        nonce = 34.toBigInteger(),
+        runtimeVersion = RuntimeVersion(48, 4),
+        genesisHash = "e143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e".fromHex(),
+        multiChainEncryption = MultiChainEncryption.Substrate(EncryptionType.ED25519),
+        accountIdentifier = multiAddressFromId(KEYPAIR.publicKey),
+        era = Era.Mortal(64, 59),
+        blockHash = "0x1b876104c68b4a8924c098d61d2ad798761bb6fff55cca2885939ffc27ef5ecb".fromHex()
+    )
 }
