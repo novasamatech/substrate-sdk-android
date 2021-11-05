1dbc43c171fa1367210e96883fa75b07381ef083
valentunn <70131744+valentunn@users.noreply.github.com>
2021-11-05 12:58:14 +0300

    Fix/non string subscription id (#21)
    * Fix - non string subscription id
    
    * Code style
    
    * Bump versions

--- fearless-utils/src/test/java/jp/co/soramitsu/fearless_utils/wsrpc/subscription/response/SubscriptionChangeTest.kt
+++ fearless-utils/src/test/java/jp/co/soramitsu/fearless_utils/wsrpc/subscription/response/SubscriptionChangeTest.kt
@@ -24,4 +24,13 @@ class SubscriptionChangeTest {
 
         assertEquals("1lQdo0fUlXLJS9v3", subscriptionChange.subscriptionId)
     }
+
+    @Test
+    fun `should handle integer subscription id`() {
+        val serialized = "{\"jsonrpc\":\"2.0\",\"method\":\"state_storage\",\"params\":{\"subscription\":2595588254652828, \"result\":{\"block\":\"0x1deed482cbed9cec67398ca222d073f99e36c48aeba7ebfb418135f7a81c9c87\",\"changes\":[[\"0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9b82e146c42d93bbe2c219bbdfaf698482a1e637d38ab3279321e34f878cabc1fd411dd40d5fde3482601275ef189663c\",\"0x01000000000000007ae384e5ae0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\"]]}}}"
+
+        val subscriptionChange = gson.fromJson(serialized, SubscriptionChange::class.java)
+
+        assertEquals("2595588254652828", subscriptionChange.subscriptionId)
+    }
 }
