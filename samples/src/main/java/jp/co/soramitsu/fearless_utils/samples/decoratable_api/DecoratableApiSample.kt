8c3d34afacdc79791b7bec554de3e060b0993621
Valentun <sergeev@soramitsu.co.jp>
2022-02-04 00:02:49 +0300

    Return back code from sample

--- samples/src/main/java/jp/co/soramitsu/fearless_utils/samples/decoratable_api/DecoratableApiSample.kt
+++ samples/src/main/java/jp/co/soramitsu/fearless_utils/samples/decoratable_api/DecoratableApiSample.kt
@@ -3,9 +3,16 @@ package jp.co.soramitsu.fearless_utils.samples.decoratable_api
 import com.google.gson.Gson
 import jp.co.soramitsu.fearless_utils.decoratable_api.SubstrateApi
 import jp.co.soramitsu.fearless_utils.decoratable_api.tx.invoke
+import jp.co.soramitsu.fearless_utils.encrypt.Keyring
 import jp.co.soramitsu.fearless_utils.gson_codec.GsonCodec
+import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.balances.balances
+import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.balances.transfer
+import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.staking.historyDepth
 import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.staking.ledger
 import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.staking.staking
+import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.types.MultiAddress
+import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.utility.batch
+import jp.co.soramitsu.fearless_utils.samples.decoratable_api.derive.utility.utility
 import jp.co.soramitsu.fearless_utils.ss58.SS58Encoder.toAccountId
 import jp.co.soramitsu.fearless_utils.wsrpc.SocketService
 import jp.co.soramitsu.fearless_utils.wsrpc.logging.Logger
@@ -40,19 +47,19 @@ class DecoratableApiSample {
             typesJsons = listOf(types)
         )
 
-//        val account = Keyring.sampleAccount().getOrThrow()
-//        val accountId = api.options.accountIdentifierConstructor.identifier(account).accountId
-//        val targetAddress = MultiAddress.Id(accountId)
-//
-//        val feeInfo = api.tx.utility.batch(
-//            api.tx.balances.transfer(targetAddress, 123.toBigInteger()),
-//            api.tx.balances.transfer(targetAddress, 123.toBigInteger()),
-//        )
-//            .paymentInfo(account)
-//        println(feeInfo.partialFee)
-//
-//        val historyDepth = api.query.staking.historyDepth()
-//        print(historyDepth)
+        val account = Keyring.sampleAccount().getOrThrow()
+        val accountId = api.options.accountIdentifierConstructor.identifier(account).accountId
+        val targetAddress = MultiAddress.Id(accountId)
+
+        val feeInfo = api.tx.utility.batch(
+            api.tx.balances.transfer(targetAddress, 123.toBigInteger()),
+            api.tx.balances.transfer(targetAddress, 123.toBigInteger()),
+        )
+            .paymentInfo(account)
+        println(feeInfo.partialFee)
+
+        val historyDepth = api.query.staking.historyDepth()
+        print(historyDepth)
 
         val ledger = api.query.staking.ledger("5E7C1NtJhfztSa4iKf8qYw1Ps88LbTTnjE65yUcf6Q9FZwqT".toAccountId())
         print(ledger)
