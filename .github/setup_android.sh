9c72f60ba20daa026b22fcc63357f1849eb18eea
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-10-02 11:00:20 +0300

    remove quite

--- .github/setup_android.sh
+++ .github/setup_android.sh
@@ -8,14 +8,14 @@ ANDROID_SDK_HOME="$ANDROID_HOME"
 ANDROID_NDK_HOME="$ANDROID_NDK/android-ndk-$ANDROID_NDK_VERSION"
 
 # Download and install Android SDK Tools
-wget --quiet --output-document=sdk-tools.zip \
+wget  --output-document=sdk-tools.zip \
     "https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_TOOLS_VERSION}.zip"
 mkdir --parents "$ANDROID_HOME"
 unzip -q sdk-tools.zip -d "$ANDROID_HOME"
 rm --force sdk-tools.zip
 
 # Download and install Android NDK
-wget --quiet --output-document=android-ndk.zip \
+wget  --output-document=android-ndk.zip \
     "http://dl.google.com/android/repository/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.zip"
 mkdir --parents "$ANDROID_NDK_HOME"
 unzip -q android-ndk.zip -d "$ANDROID_NDK"
@@ -26,50 +26,50 @@ mkdir --parents "$HOME/.android/"
 echo '### User Sources for Android SDK Manager' > "$HOME/.android/repositories.cfg"
 
 # Download and install Android platforms
-wget --quiet --output-document=platforms.zip \
+wget --output-document=platforms.zip \
     "https://dl.google.com/android/repository/platforms;android-30"
 unzip -q platforms.zip -d "$ANDROID_HOME"
 rm --force platforms.zip
 
-wget --quiet --output-document=platforms.zip \
+wget --output-document=platforms.zip \
     "https://dl.google.com/android/repository/platforms;android-29"
 unzip -q platforms.zip -d "$ANDROID_HOME"
 rm --force platforms.zip
 
 # Download and install Android platform tools
-wget --quiet --output-document=platform-tools.zip \
+wget --output-document=platform-tools.zip \
     "https://dl.google.com/android/repository/platform-tools"
 unzip -q platform-tools.zip -d "$ANDROID_HOME"
 rm --force platform-tools.zip
 
 # Download and install Android build tools
-wget --quiet --output-document=build-tools.zip \
+wget --output-document=build-tools.zip \
     "https://dl.google.com/android/repository/build-tools;30.0.0"
 unzip -q build-tools.zip -d "$ANDROID_HOME"
 rm --force build-tools.zip
 
-wget --quiet --output-document=build-tools.zip \
+wget --output-document=build-tools.zip \
     "https://dl.google.com/android/repository/build-tools;29.0.3"
 unzip -q build-tools.zip -d "$ANDROID_HOME"
 rm --force build-tools.zip
 
-wget --quiet --output-document=build-tools.zip \
+wget --output-document=build-tools.zip \
     "https://dl.google.com/android/repository/build-tools;29.0.2"
 unzip -q build-tools.zip -d "$ANDROID_HOME"
 rm --force build-tools.zip
 
-wget --quiet --output-document=build-tools.zip \
+wget --output-document=build-tools.zip \
     "https://dl.google.com/android/repository/build-tools;28.0.3"
 unzip -q build-tools.zip -d "$ANDROID_HOME"
 rm --force build-tools.zip
 
-wget --quiet --output-document=build-tools.zip \
+wget --output-document=build-tools.zip \
     "https://dl.google.com/android/repository/build-tools;28.0.2"
 unzip -q build-tools.zip -d "$ANDROID_HOME"
 rm --force build-tools.zip
 
 # Download and install Android emulator
-wget --quiet --output-document=emulator.zip \
+wget --output-document=emulator.zip \
     "https://dl.google.com/android/repository/emulator"
 unzip -q emulator.zip -d "$ANDROID_HOME"
 rm --force emulator.zip
